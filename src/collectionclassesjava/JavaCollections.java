163748628ae922d6ce03c1ec438fa2552ff33022
prasad.guthula <prasad.guthula@E1280F1D.lsz.ads.valuelabs.net>
2018-01-09 11:39:48 +0530

    JavaCollectionsPrograms

---
+++ src/collectionclassesjava/JavaCollections.java
@@ -0,0 +1,20 @@
+package collectionclassesjava;
+
+public class JavaCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//List-->ArrayList, LinkedList, Vector and Stack
+	//ArrayList: 
+	
+	public static void listExample() {
+		
+		
+		
+		
+	}
+
+}
