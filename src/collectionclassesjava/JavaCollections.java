197c7e4442c8315dc45bcd6953548b492ec4e05f
prasad.guthula <prasad.guthula@E1280F1D.lsz.ads.valuelabs.net>
2018-01-11 11:48:57 +0530

    ArrayList Example

--- src/collectionclassesjava/JavaCollections.java
+++ src/collectionclassesjava/JavaCollections.java
@@ -1,17 +1,35 @@
 package collectionclassesjava;
 
+import java.util.ArrayList;
+
 public class JavaCollections {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		arrayListExample();
 
 	}
 	
-	//List-->ArrayList, LinkedList, Vector and Stack
-	//ArrayList: 
-	
-	public static void listExample() {
+	//Set-->
+	public static void setExample() {
 		
+	}
+	
+	//List-->ArrayList, LinkedList, Vector and Stack
+	//ArrayList: implements List and RandomAccess(MarkerInterface) interfaces
+		//Quick access in search and slow when we delete and insert compared to LinkedList
+	public static void arrayListExample() {
+		
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("Mobile");
+		al.add("SIM");
+		al.add("Mobile");
+		al.add("Laptop");
+		al.add("Desktop");
+		
+		System.out.println(al.get(0));
+		System.out.println(al.get(1));
+		System.out.println(al.get(4));
 		
 		
 		
